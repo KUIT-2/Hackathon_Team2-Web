55d8f812374ffcea8c7ec7b0d2cbe4df9be4b0fb
unique0902 <rla0591@naver.com>
2023-12-20 05:03:41 +0900

    Feat: bottomsheet back navigate

--- src/components/BottomSheet/BottomSheet.jsx
+++ src/components/BottomSheet/BottomSheet.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { BottomSheetWrapper, ChildrenWrapper } from "./BottomSheet.styles";
+import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router';
+import { BottomSheetWrapper, ChildrenWrapper } from './BottomSheet.styles';
 
 export default function BottomSheet({ heightPer, children }) {
+  const wrapperRef = useRef(null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        navigate(-1);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [wrapperRef, navigate]);
   return (
     <BottomSheetWrapper>
-      <ChildrenWrapper heightper={heightPer}>{children}</ChildrenWrapper>
+      <ChildrenWrapper ref={wrapperRef} heightper={heightPer}>
+        {children}
+      </ChildrenWrapper>
     </BottomSheetWrapper>
   );
 }
